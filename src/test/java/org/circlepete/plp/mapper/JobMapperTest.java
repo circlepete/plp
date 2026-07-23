13f6d4191db8b77990de8fa1271b9518c3f840f4
peter <git@workhorse-pi.org>
2026-07-23 21:20:57 +0200

    additional test configuration; add mapstruct unit test

--- src/test/java/org/circlepete/plp/mapper/JobMapperTest.java
+++ src/test/java/org/circlepete/plp/mapper/JobMapperTest.java
@@ -1,13 +1,35 @@
 package org.circlepete.plp.mapper;
 
+import org.circlepete.plp.dto.JobRequest;
+import org.circlepete.plp.entity.Job;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = JobMapperImpl.class)
 public class JobMapperTest {
+
+    @Autowired
+    JobMapper mapper;
+
     @Test
     public void shouldMapFromRequestToJob() {
-        assertSame("Hello", "Hello");
-    }
+        // given
+        JobRequest request = new JobRequest("some name", "some details");
 
+        // when
+        Job result = mapper.toJob(request);
+
+        // then
+        assertSoftly(softAssertions -> {
+            softAssertions.assertThat(request.name()).isEqualTo(result.getName());
+            softAssertions.assertThat(request.details()).isEqualTo(result.getDetails());
+            softAssertions.assertThat(result.getId()).isNotNull();
+        });
+    }
 }
